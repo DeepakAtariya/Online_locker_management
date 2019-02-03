e1afd57d55f37e35c24c8ac824aab68fb66e5756
DeepakAtariya <Deepakkumaratariya@gmail.com>
2019-02-03 23:05:25 +0530

    customer_app is created and mapping is sorted.....

--- online_locker_mang/customer/views.py
+++ online_locker_mang/customer/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse 
+
 
 # Create your views here.
+def home(request):
+    return HttpResponse('<h1>Cutomer</<h1>')
+
+def register(request):
+    # return HttpResponse('<h1>Register</<h1>')
+    return render(request, 'customer/register.html')
--- online_locker_mang/online_locker_mang/settings.py
+++ online_locker_mang/online_locker_mang/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'customer.apps.CustomerConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
