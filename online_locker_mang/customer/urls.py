ed5ced9e97f4e75186c971961698ffc8a269bba6
DeepakAtariya <deepakkumaratariya@gmail.com>
2019-02-05 10:51:13 +0530

    System_O checked

--- online_locker_mang/customer/urls.py
+++ online_locker_mang/customer/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name = 'customer-home'),
-    path('register', views.register, name = 'customer-register')
+    path('register', views.register, name = 'customer-register'),
+    path('login', views.register, name = 'customer-login')
     
 ]
