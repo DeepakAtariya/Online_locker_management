4204555c30135446a392fd5794e37d1fdb9ee708
DeepakAtariya <Deepakkumaratariya@gmail.com>
2019-03-02 18:59:37 +0530

    project is decided to be migrated

--- online_locker_mang/customer/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.home, name = 'customer-home'),
-    path('register', views.register, name = 'customer-register')
-    
-]
