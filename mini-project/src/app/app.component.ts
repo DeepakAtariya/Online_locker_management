9cbeadf44890a04dd9dc798d5962131172c84f7a
DeepakAtariya <Deepakkumaratariya@gmail.com>
2019-03-11 00:22:55 +0530

    APi is build successfully; tested with angular

--- mini-project/src/app/app.component.ts
+++ mini-project/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'mini-project';
+
+  kajal_data : any;
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit(){
+    this.http.get('http://localhost:8080/api2/miniproject/AuthorService/test')
+    .subscribe((data)=>{
+      console.log(data);
+
+      this.kajal_data = data;
+    });
+  
+  }
 }
+
